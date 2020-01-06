29e8c61b3e35ce9c25550ff827a8477271958cd6
Girija Viswanathan <58586934+GirijaViswa@users.noreply.github.com>
2020-01-06 14:50:16 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do 
+        "Hello, World!"
+    end
+    get '/favorite-song' do 
+        "My favorite song is __"
+    end
+    get '/hometown' do 
+        "My hometown is "
+    end
+    get '/name' do 
+        "My name is __"
+    end
+
 end
